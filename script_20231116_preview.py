2758814e61391c725b534e55f6617e55b5cd6837
RuLzkin <kuronekokaito@gmail.com>
2023-11-29 06:47:19 +0900

    ファイル位置変更

--- script_20231116_preview.py
+++ script_20231116_preview.py
@@ -1,6 +1,6 @@
 from module_load_exr import preview_exr
 
 
-preview_exr("./testfiles/20231116/CMS_EOTFcorrected_Shoot_Achievable.exr")
-preview_exr("./testfiles/20231116/For_inner_Calib_chart.0.exr")
-preview_exr("./testfiles/20231116/For_inner_Calib_rgbw.0.exr", show=True)
+preview_exr("C:/Dropbox/TOEI/20231115_inner_correction/CMS_EOTFcorrected_Shoot_Achievable.exr")
+preview_exr("C:/Dropbox/TOEI/20231115_inner_correction/For_inner_Calib_chart.0.exr")
+preview_exr("C:/Dropbox/TOEI/20231115_inner_correction/For_inner_Calib_rgbw.0.exr", show=True)
