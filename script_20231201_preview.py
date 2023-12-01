b35d5e31e12d084d6176c976e0c499bd9dd53462
RuLzkin <kuronekokaito@gmail.com>
2023-12-01 16:11:00 +0900

    カラーチェッカーデータ確認

---
+++ script_20231201_preview.py
@@ -0,0 +1,5 @@
+from module_load_exr import preview_exr
+
+
+preview_exr(r"C:\Dropbox\TOEI\20231201_ColorChecker\ACES2065-1_ColorChecker2014.exr")
+preview_exr(r"C:\Dropbox\TOEI\20231201_ColorChecker\ACEScg_ColorChecker2014.exr", show=True)
