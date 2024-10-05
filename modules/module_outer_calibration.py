5fab0a1298872bcd1fa701c01e2a861ae21bf7f6
RuLzkin <kuronekokaito@gmail.com>
2024-10-06 01:12:52 +0900

    関数のヒント追記とCC関数のカプセル化

--- modules/module_outer_calibration.py
+++ modules/module_outer_calibration.py
@@ -6,16 +6,61 @@ import cv2
 from module_load_exr import load_exr
 
 
-def calclate_matrix_q(list_mat_s: list, mat_cc: ndarray, w_avg: ndarray) -> ndarray:
-    q_initiate = np.eye(3).reshape(9)
+def calculate_matrix_q(list_mat_s: list, mat_cc: ndarray, w_avg: ndarray) -> ndarray:
+    """Calculate Matrix Q
+
+        Args:
+            list_mat_s: list[ndarray],
+                List of matrices, each representing a color patch illuminated by R, G, B light sources. Length equals the number of color checker patches. Each matrix column is a color vector.
+            mat_cc: ndarray, RGB values of color checker patches in the target image. 3 rows (R, G, B) and columns equal to the number of patches.
+            w_avg: ndarray, RGB values of the whitest patch in the color checker.
+
+        Returns:
+            ndarray, 3 * 3 matrix, Q matrix for Outer Frustum
+
+        Examples:
+            >>> mat_q = calculate_matrix_q(list_mat_s, mat_cc, w_avg)
+            [[1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 1]]
+    """
+    q_initiate: ndarray = np.eye(3).reshape(9)
     param = least_squares(func_fit, q_initiate, bounds=(-2, 2), args=(list_mat_s, mat_cc, w_avg))
-    q_flat = param.x
-    mat_q = q_flat.reshape((3, 3))
+    q_flat: ndarray = param.x
+    mat_q: ndarray = q_flat.reshape((3, 3))
     return mat_q
 
 
-def extract_colors_from_cc(img: np.ndarray, str_title: str = "", show_preview: bool = False):
-    mat_extracted_cc = np.zeros((4, 6, 3))
+def func_fit(q: np.ndarray, list_mat_s, mat_cc, w_avg) -> ndarray:
+    mat_q: ndarray = q.reshape((3, 3))
+    val: np.ndarray = np.zeros((3, 24))
+
+    for _i in range(len(list_mat_s)):
+        val[:, _i] = (list_mat_s[_i] @ mat_q @ w_avg[:, None] - mat_cc[:, _i:_i + 1]).flatten()
+
+    return val.flatten()
+
+
+def colorchecker2colors(img_cc, pts_src, pts_dst, shape_trans, str_title="test", show_preview=True) -> ndarray:
+    """(wrapper) Extract Colors from Color Checker
+
+    Args:
+        img_cc: ndarray, input image
+        pts_src, pts_dst: cv2.getPerspectiveTransform(pts_src, pts_dst)
+        shape_trans: dsize args for cv2.warpPerspective()
+        str_title: str, for preview
+        show_preview: bool, show preview or not
+
+    Returns:
+        ndarray, shape(row_cc, column_cc, [R, G, B])
+    """
+    img_trans = transform_pers(img_cc, pts_src, pts_dst, shape_trans)
+    mat_extracted = extract_colors_from_cc(img_trans, str_title, show_preview=show_preview)
+    return mat_extracted
+
+
+def extract_colors_from_cc(img: ndarray, str_title: str = "", show_preview: bool = False) -> ndarray:
+    mat_extracted_cc: ndarray = np.zeros((4, 6, 3))
     shape_trans = img.shape
     _width_area = np.min(shape_trans[:2]) * 0.35 / 6
 
@@ -41,20 +86,10 @@ def extract_colors_from_cc(img: np.ndarray, str_title: str = "", show_preview: b
     return mat_extracted_cc
 
 
-def transform_pers(img, pts_src, pts_dst, shape_trans):
-    mat_pers = cv2.getPerspectiveTransform(pts_src, pts_dst)
-    img_trans = cv2.warpPerspective(img, mat_pers, shape_trans)
+def transform_pers(img, pts_src, pts_dst, shape_trans) -> ndarray:
+    mat_pers: ndarray = cv2.getPerspectiveTransform(pts_src, pts_dst)
+    img_trans: ndarray = cv2.warpPerspective(img, mat_pers, shape_trans)
     return img_trans
-
-
-def func_fit(q: np.ndarray, list_mat_s, mat_cc, w_avg):
-    mat_q = q.reshape((3, 3))
-    val: np.ndarray = np.zeros((3, 24))
-
-    for _i in range(len(list_mat_s)):
-        val[:, _i] = (list_mat_s[_i] @ mat_q @ w_avg[:, None] - mat_cc[:, _i:_i + 1]).flatten()
-
-    return val.flatten()
 
 
 def comparison(mat_extracted_ref, mat_extracted_w, img_cc_w, img_cc_refer, img_calibrated):
@@ -121,7 +156,7 @@ if __name__ == "__main__":
     img_cc_r = load_exr("CAL_OuterFrustum_v01_dR.exr")
     img_cc_g = load_exr("CAL_OuterFrustum_v01_dG.exr")
     img_cc_b = load_exr("CAL_OuterFrustum_v01_dB.exr")
-    img_cc_refer = load_exr("OuterCheck_Ref.exr")
+    img_cc_ref = load_exr("OuterCheck_Ref.exr")
 
     shape_trans = int(img_cc_w.shape[1]), int(img_cc_w.shape[0])
 
@@ -132,17 +167,11 @@ if __name__ == "__main__":
     pts_dst = np.float32(
         [[0, 0], [shape_trans[0], 0], [0, shape_trans[1]], [shape_trans[0], shape_trans[1]]])
 
-    img_trans_w = transform_pers(img_cc_w, pts_src, pts_dst, shape_trans)
-    img_trans_r = transform_pers(img_cc_r, pts_src, pts_dst, shape_trans)
-    img_trans_g = transform_pers(img_cc_g, pts_src, pts_dst, shape_trans)
-    img_trans_b = transform_pers(img_cc_b, pts_src, pts_dst, shape_trans)
-    img_trans_ref = transform_pers(img_cc_refer, pts_src_ref, pts_dst, shape_trans)
-
-    mat_extracted_w = extract_colors_from_cc(img_trans_w, "CAL_OuterFrustum_v01_dW.exr", show_preview=True)
-    mat_extracted_r = extract_colors_from_cc(img_trans_r, "CAL_OuterFrustum_v01_dR.exr", show_preview=True)
-    mat_extracted_g = extract_colors_from_cc(img_trans_g, "CAL_OuterFrustum_v01_dG.exr", show_preview=True)
-    mat_extracted_b = extract_colors_from_cc(img_trans_b, "CAL_OuterFrustum_v01_dB.exr", show_preview=True)
-    mat_extracted_ref = extract_colors_from_cc(img_trans_ref, "OuterCheck_Ref.exr", show_preview=True)
+    mat_extracted_w = colorchecker2colors(img_cc_w, pts_src, pts_dst, shape_trans, "CAL_OuterFrustum_v01_dW.exr", show_preview=True)
+    mat_extracted_r = colorchecker2colors(img_cc_r, pts_src, pts_dst, shape_trans, "CAL_OuterFrustum_v01_dR.exr", show_preview=True)
+    mat_extracted_g = colorchecker2colors(img_cc_g, pts_src, pts_dst, shape_trans, "CAL_OuterFrustum_v01_dG.exr", show_preview=True)
+    mat_extracted_b = colorchecker2colors(img_cc_b, pts_src, pts_dst, shape_trans, "CAL_OuterFrustum_v01_dB.exr", show_preview=True)
+    mat_extracted_ref = colorchecker2colors(img_cc_ref, pts_src_ref, pts_dst, shape_trans, "OuterCheck_Ref.exr", show_preview=True)
 
     mat_extracted_ref = mat_extracted_ref / np.max(mat_extracted_ref)
 
@@ -162,11 +191,11 @@ if __name__ == "__main__":
             vec_rgb_b[:, _i],
         ], axis=0).reshape((3, 3)))
 
-    mat_q = calclate_matrix_q(list_mat_s, mat_cc, w_avg)
+    mat_q = calculate_matrix_q(list_mat_s, mat_cc, w_avg)
 
     img_calibrated = func_fit(mat_q.ravel(), list_mat_s, np.zeros_like(mat_cc), w_avg).reshape((3, 4, 6)).transpose((1, 2, 0))
 
-    comparison(mat_extracted_ref, mat_extracted_w, img_cc_w, img_cc_refer, img_calibrated)
+    comparison(mat_extracted_ref, mat_extracted_w, img_cc_w, img_cc_ref, img_calibrated)
 
     print("matrix Q:\n", mat_q)
     print("norm(before)", np.linalg.norm(mat_extracted_w - mat_extracted_ref))
